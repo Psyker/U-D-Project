ffc82d4cc59645a4a3c6b12a037985309277bb48
Trapdafifou <thibaut.marsal@gmail.com>
2017-04-19 17:16:06 +0200

    [ADD:app/Resources] smooth scroll

--- web/assets/app/js/animation/smoothScroll.js
+++ web/assets/app/js/animation/smoothScroll.js
@@ -2,12 +2,17 @@
  * Created by Thib on 18/04/2017.
  */
 let smoothScroll = {
-    setLinkScroll (element) {
-        window.scrollTo(0, element.offsetTop)
+    setLinkScroll (element, offset) {
+        window.scrollTo(0, element.offsetTop + offset)
     },
     init () {
-        document.querySelectorAll('.js-scrollAnchor')
-        addEventListener('')
+        let linksTab = document.querySelectorAll('.js-scrollAnchor')
+        for (let i = 0 ; i <= linksTab.length; i++) {
+            linksTab[i].addEventListener('click', function (event) {
+                event.preventDefault();
+                setLinkScroll(linksTab[i], 0)
+            })
+        }
     }
 };
 
