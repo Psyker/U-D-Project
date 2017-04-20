48ced7f4b1a3806542ac767f08268d5cf8d23aff
renanbronchart <renan.bronchart@gmail.com>
2017-04-20 17:14:00 +0200

    [fix:Contact] Fix background contact

--- web/assets/app/js/animation/smoothScroll.js
+++ web/assets/app/js/animation/smoothScroll.js
@@ -6,9 +6,8 @@ import jump from 'jump.js'
 let smoothScroll = {
     init () {
         let linksTab = document.querySelectorAll('.navSticky__tabLink');
-        console.log(linksTab)
+
         for (let i = 0; i < linksTab.length; i++) {
-            console.log(jump)
             linksTab[i].addEventListener('click', function () {
                 jump(linksTab[i].getAttribute('href'))
             })
